edc03e0b625b2087ad78748b302b0083ed26fcad
Oleg Anashkin <oleg.anashkin@gmail.com>
2026-04-26 18:51:27 -0700

    Update trakt.tv plugin. Fixes #1767

--- plugins/trakttv.js
+++ plugins/trakttv.js
@@ -6,7 +6,7 @@ hoverZoomPlugins.push({
         // https://walter.trakt.tv/images/shows/000/189/717/posters/thumb/95911acafb.jpg.webp
         // https://walter.trakt.tv/images/shows/000/189/717/posters/full/95911acafb.jpg.webp
         var res = [];
-        $('body').on('mouseenter', 'img.real[src*="/thumb/"]', function() {
+        $('body').on('mouseenter', 'img[src*="/thumb/"]', function() {
             const img = $(this);
             const src = img.attr('src').replace("thumb", "full");
             hoverZoom.prepareLink(img.parent(), src);
